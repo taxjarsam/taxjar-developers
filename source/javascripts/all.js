2d290979e2533129084e605cbf19f36229df19dc
Jake Johnson <jake@fastdivision.com>
2015-06-12 15:49:36 -0700

    Set up lander

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,21 @@
 //= require_tree .
+//= require particles.js/particles.js
+//= require walkway.js/src/walkway.js
+
+(function() {
+  var svg = new Walkway({
+    selector: '#jar',
+    duration: 800,
+    easing: 'easeInOutCubic'
+  });
+
+  particlesJS.load('particles-js', '/javascripts/particles.json', function() {
+    var heroIcon = document.querySelector('.hero__icon');
+    setTimeout(function() {
+      heroIcon.classList.add('active');
+      svg.draw(function() {
+        document.querySelector('.hero').classList.add('active');
+      });
+    }, 100);
+  });
+})();

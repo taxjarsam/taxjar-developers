82bd36fe07004278bb3e8b1b696795f645e8d463
Jake Johnson <jake@fastdivision.com>
2015-09-17 17:22:15 -0700

    Bold active nav links and conditional for index animations

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,19 +3,28 @@
 //= require walkway.js/src/walkway.js
 
 (function() {
-  var svg = new Walkway({
-    selector: '#jar',
-    duration: 1000,
-    easing: 'easeOutCubic'
-  });
+  var path = window.location.pathname;
+  var activeLink = document.querySelector('.menu a[href="' + path + '"]');
+  
+  if (activeLink) {
+    activeLink.classList.add('active');
+  }
+  
+  if (document.querySelector('#jar')) {
+    var svg = new Walkway({
+      selector: '#jar',
+      duration: 1000,
+      easing: 'easeOutCubic'
+    });
 
-  particlesJS.load('particles-js', '/javascripts/particles.json', function() {
-    var heroIcon = document.querySelector('.hero__icon');
-    setTimeout(function() {
-      heroIcon.classList.add('active');
-      svg.draw(function() {
-        document.querySelector('.hero').classList.add('active');
-      });
-    }, 100);
-  });
+    particlesJS.load('particles-js', '/javascripts/particles.json', function() {
+      var heroIcon = document.querySelector('.hero__icon');
+      setTimeout(function() {
+        heroIcon.classList.add('active');
+        svg.draw(function() {
+          document.querySelector('.hero').classList.add('active');
+        });
+      }, 100);
+    });  
+  }
 })();

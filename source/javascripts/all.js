d3f4e1635acc427c6435eef8f88daaacd241f89c
Jake Johnson <jake@fastdivision.com>
2017-09-13 14:09:49 -0700

    Add scrollspy for guide TOC items

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,15 +1,21 @@
 //= require jquery-smooth-scroll/jquery.smooth-scroll
 //= require fastclick
 //= require particles.js/particles.js
+//= require scrollspy
 //= require_tree ./components
 
 (function($) {
-  FastClick.attach(document.body);
-  $('.docs-toc a').smoothScroll({ offset: -90 });
-
   var path = window.location.pathname;
   var bodyClass = document.body.classList[0];
   var activeLink = document.querySelector('.menu a[href="' + path + '"], .menu a[href*="' + bodyClass + '"]');
+  var scrollSpy = new ScrollSpy('.docs', {
+    nav: '.docs-toc li > a',
+    className: 'active'
+  });
+
+  FastClick.attach(document.body);
+
+  $('.docs-toc a').smoothScroll({ offset: -90 });
 
   if (activeLink) {
     activeLink.classList.add('active');

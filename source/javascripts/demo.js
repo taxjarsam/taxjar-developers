eeb3754fe4e056a6828306981c6efbfe14d0ebc5
Jake Johnson <jake@taxjar.com>
2021-05-10 14:08:55 -0700

    Update Mapbox access token

--- source/javascripts/demo.js
+++ source/javascripts/demo.js
@@ -6,7 +6,7 @@
 //= require_tree ./demo/vendor
 //= require_tree ./demo/components
 
-L.mapbox.accessToken = 'pk.eyJ1IjoiamFrZXRheGphciIsImEiOiJjaWo0cWJkM3cwMDNqdXJrcmM0MWt4enNuIn0.16PIitx_ccoMZEIEnkG7pQ';
+L.mapbox.accessToken = 'pk.eyJ1IjoiamFrZXRheGphciIsImEiOiJja28zNDA4Y3cwemg2MnBuc2p5Y3RyOGRtIn0.xcKzob7XS8zBmXblI6WZzQ';
 
 ReactDOM.render(
   React.createElement(Sandbox, null),
